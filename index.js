eaa065ceab225c16c32f361d471d8316fb69795a
Samuel Novaes <samuel.novaes.96@gmail.com>
2018-08-02 03:40:50 -0300

    Some fixes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const compression = require('compression')
 const serveStatic = require('serve-static')
+const path = require('path')
 
 module.exports = (data, port, cb) => {
 	const app = express()
 	app.use(compression())
-	app.use(serveStatic('static'))
+	app.use(serveStatic(path.join(__dirname, 'static')))
 	app.get('/data', (req, res) => {
 		res.json(data)
 	})
